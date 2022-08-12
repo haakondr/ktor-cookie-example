82991f5c030b5c8441fd90278c1a67d58e197852
Håkon Drolsum Røkenes <haakon.rokenes@origo.oslo.kommune.no>
2022-08-12 10:17:26 +0200

    added a simple minimal example with two cookies
    - one cookie in plaintext
    - one cookie encrypted
    
    encrypted cookie 270 bytes
    plaintext cookie 127 bytes

---
+++ README.md
@@ -0,0 +1,16 @@
+# Ktor cookies
+
+Cookies size is too large :( minimal example
+
+![The cookies are too damn large](https://www.washingtonpost.com/blogs/the-fix/files/2015/07/PHO-10Oct19-261389.jpg)
+
+## Running
+
+```
+./gradlew build
+./gradlew run
+```
+
+access the web application at http://localhost:8080/ and check experience how your cookies grow in size when encrypted:
+
+![cookie size doubled](./cookie-size.png)
--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,16 +1,31 @@
 package com.example.plugins
 
-import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import io.ktor.server.sessions.*
+import io.ktor.util.hex
 
 fun Application.configureRouting() {
+    data class SessionCleartext(val text: String)
+    data class SessionEncrypted(val text: String)
+
+    install(Sessions) {
+        cookie<SessionCleartext>("session_cleartext")
+        cookie<SessionEncrypted>("session_encrypted") {
+            val secretEncryptKey = hex("00112233445566778899aabbccddeeff")
+            val secretSignKey = hex("6819b57a326945c1968f45236589")
+            transform(SessionTransportTransformerEncrypt(secretEncryptKey, secretSignKey))
+        }
+    }
 
     routing {
         get("/") {
-            call.respondText("Hello World!")
+            val cookieContent = "This is my cookie! There are many like it, but this one is mine"
+            call.sessions.set("session_cleartext", SessionCleartext(cookieContent))
+            call.sessions.set("session_encrypted", SessionEncrypted(cookieContent))
+
+            call.respondText("cookies updated, check devtools -> application -> Cookies")
         }
     }
 }
