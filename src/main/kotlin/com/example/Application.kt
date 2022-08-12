c99b7c2c10e8cbab3abe1246c01a81252fc5acf7
Håkon Drolsum Røkenes <haakon.rokenes@origo.oslo.kommune.no>
2022-08-12 09:59:59 +0200

    init ktor skeleton generated by intellij

---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,12 @@
+package com.example
+
+import com.example.plugins.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureSecurity()
+        configureRouting()
+    }.start(wait = true)
+}
